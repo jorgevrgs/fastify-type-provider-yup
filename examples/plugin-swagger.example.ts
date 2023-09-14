88c8e095c804ec6625942e632e6bc614051478e0
Jorge Vargas <jorgevargasla@gmail.com>
2023-09-13 21:52:14 -0500

    docs: add swagger example

---
+++ examples/plugin-swagger.example.ts
@@ -0,0 +1,60 @@
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
+import Fastify from 'fastify';
+import * as yup from 'yup';
+import { YupTypeProvider } from '../src/type-provider';
+import { validatorCompiler } from '../src/validator-compiler';
+import { serializerCompiler } from '../src/serializer-compiler';
+import { jsonSchemaTransformer } from '../src/json-transformer';
+import { extendSchema } from '@sodaru/yup-to-json-schema';
+import { Schema, addMethod } from 'yup';
+
+extendSchema({ addMethod, Schema });
+
+const app = Fastify({ logger: true });
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'SampleApi',
+      description: 'Sample backend service',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransformer,
+});
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+});
+
+app.after(() => {
+  app.withTypeProvider<YupTypeProvider>().route({
+    url: '/',
+    method: 'POST',
+    schema: {
+      description: 'Description details',
+      tags: ['home'],
+      body: yup.object({
+        page: yup.number().default(1),
+      }),
+      response: {
+        200: yup.object({
+          page: yup.string().example('1'),
+        }),
+      },
+    },
+    handler: async (request, reply) => {
+      const { page } = request.body;
+
+      return {
+        page: String(page),
+      };
+    },
+  });
+});
+
+app.listen({ port: 8080, host: '0.0.0.0' });
