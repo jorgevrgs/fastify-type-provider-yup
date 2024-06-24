3b83ec5db3af83fb44d6ceff608acba8d5df0863
Jorge Vargas <jorge.vargas@zemoga.com>
2024-06-24 11:07:24 -0500

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./json-transformer";
 export * from "./plugin";
 export * from "./serializer-compiler";
 export * from "./type-provider";
+export type * from "./types";
 export * from "./validator-compiler";
